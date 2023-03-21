1859991199cab53348a9ae7e2326979efc0d6f41
Myles Reyburn <mylesreyburn@gmail.com>
2023-03-20 22:25:16 -0600

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+
+class Anagram:
+
+    def __init__(self, word) -> None:
+        self.word = word
+
+    def match(self, list):
+        match_list = []
+        for word in list:
+            word_deconstructed = [letter for letter in word]
+            search_word_deconst = [letter for letter in self.word]
+
+            if sorted(word_deconstructed) == sorted(search_word_deconst):
+                match_list.append(word)
+
+        return match_list
